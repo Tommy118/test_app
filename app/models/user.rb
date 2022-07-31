f8fb921509797e52629a07c31f57e52c4b91d39f
Michał <michal.swider@icloud.com>
2022-07-31 14:12:25 +0200

    create-user-test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  before_save  { self.user_email = user_email.downcase }
   has_many :articles
   validates :username, presence:true, uniqueness: { case_sensitive: false },
             length: {minimum: 5, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :user_email, presence:true, uniqueness: { case_sensitive: false },
             length: {maximum: 100}, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end

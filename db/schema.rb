b709295a4b562964ef4a4299685474bbd182fb0f
Michał <michal.swider@icloud.com>
2022-08-01 16:02:14 +0200

    just before automated tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_131648) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_124022) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_131648) do
     t.datetime "updated_at", null: false
     t.string "user_email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end

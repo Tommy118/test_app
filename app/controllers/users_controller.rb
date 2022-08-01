c5b1d2f0238f54b026d32d77dcc131d4bc960ac4
Michał <michal.swider@icloud.com>
2022-08-01 11:51:11 +0200

    FIX !!!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,32 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: [:show, :edit, :update]
 
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
   def show
-    @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  # GET /users/new
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
   def new
     @user = User.new
-    if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog, you have successfully signup"
-      redirect_to article_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to @user
     else
-      render 'new'
+      render 'edit'
     end
   end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -42,38 +37,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :user_email, :password)
   end
-end
 
-
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
+  def set_user
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] = "Your account information was successfully updated"
-      redirect_to @user
-    else
-      render 'edit'
-    end
   end
 
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :user_email, :password)
-    end
+end
